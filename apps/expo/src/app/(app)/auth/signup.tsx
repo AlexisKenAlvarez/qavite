d043a29cb5d81ace33a9d47934f191206be3672f
Alexis Ken Alvarez <alexisken1432@gmail.com>
2024-04-11 11:31:46 +0800

    added privacy modal

--- apps/expo/src/app/(app)/auth/signup.tsx
+++ apps/expo/src/app/(app)/auth/signup.tsx
@@ -1,43 +1,56 @@
 import { useState } from "react";
-import { Pressable, ScrollView, Text, TextInput, View } from "react-native";
+import {
+  Modal,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
+import { BlurView } from "expo-blur";
 import { validateEmailDomain } from "@/lib/helpers";
 import { InputStyles, primaryColor, styles, textStyles } from "@/lib/styles";
 import { supabase } from "@/trpc/supabase";
+import { api } from "@/utils/api";
+import { AntDesign } from "@expo/vector-icons";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
-import { api } from "@/utils/api";
+
+const formArr = [
+  {
+    label: "firstname",
+    placeholder: "Juan",
+    isPassword: false,
+  },
+
+  {
+    label: "lastname",
+    placeholder: "Dela Cruz",
+    isPassword: false,
+  },
+  {
+    label: "email",
+    placeholder: "juandelacruz@cvsu.edu.ph",
+    isPassword: false,
+  },
+  {
+    label: "password",
+    placeholder: "Password",
+    isPassword: true,
+  },
+  {
+    label: "confirmPassword",
+    placeholder: "Confirm Password",
+    isPassword: true,
+  },
+];
 
 const Signup = () => {
-  const formArr = [
-    {
-      label: "firstname",
-      placeholder: "Juan",
-      isPassword: false,
-    },
+  const [modalVisible, setModalVisible] = useState(false);
 
-    {
-      label: "lastname",
-      placeholder: "Dela Cruz",
-      isPassword: false,
-    },
-    {
-      label: "email",
-      placeholder: "juandelacruz@cvsu.edu.ph",
-      isPassword: false,
-    },
-    {
-      label: "password",
-      placeholder: "Password",
-      isPassword: true,
-    },
-    {
-      label: "confirmPassword",
-      placeholder: "Confirm Password",
-      isPassword: true,
-    },
-  ];
   const [agreed, setAgreed] = useState(false);
   const createUserMutation = api.auth.createUser.useMutation({
     onError: (error) => {
@@ -91,8 +104,6 @@ const Signup = () => {
       console.error("Failed to sign in.");
       return;
     }
-
-    console.log(values);
   }
 
   return (
@@ -157,27 +168,44 @@ const Signup = () => {
         </View>
 
         <View style={[styles.center, { marginTop: 20, flexDirection: "row" }]}>
-          <BouncyCheckbox
-            size={20}
-            fillColor="#008400"
-            iconStyle={{
-              borderColor: "#008400",
-              marginRight: 0,
-              paddingRight: 0,
+          <View
+            style={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
             }}
-            innerIconStyle={{ borderWidth: 1.5 }}
-            isChecked={agreed}
-            onPress={(isChecked: boolean) => {
-              setAgreed(isChecked);
-            }}
-          />
+          >
+            <BouncyCheckbox
+              size={20}
+              fillColor="#008400"
+              iconStyle={{
+                borderColor: "#008400",
+                marginRight: 0,
+                paddingRight: 0,
+              }}
+              innerIconStyle={{ borderWidth: 1.5 }}
+              isChecked={agreed}
+              onPress={(isChecked: boolean) => {
+                setAgreed(isChecked);
+              }}
+            />
 
-          <Text style={[textStyles.lg, { marginLeft: -4 }]}>
-            I agree to the{" "}
-            <Text style={{ fontWeight: "bold", color: primaryColor }}>
-              Terms of Service
+            <Text style={[textStyles.lg, { marginLeft: -4 }]}>
+              I agree to the{" "}
             </Text>
-          </Text>
+            <Pressable onPress={() => setModalVisible(true)}>
+              <Text
+                style={[
+                  textStyles.lg,
+                  { fontWeight: "bold", color: primaryColor },
+                ]}
+              >
+                Terms of Service
+              </Text>
+            </Pressable>
+          </View>
         </View>
 
         <Pressable
@@ -187,9 +215,68 @@ const Signup = () => {
         >
           <Text style={styles.buttonText}>Continue</Text>
         </Pressable>
+
+        <Modal visible={modalVisible} transparent>
+          <BlurView
+            experimentalBlurMethod={"dimezisBlurView"}
+            style={modalStyle.container}
+          >
+            <View style={modalStyle.modal}>
+              <Text
+                style={[
+                  textStyles.xl,
+                  { fontWeight: "bold", textAlign: "center" },
+                ]}
+              >
+                Privacy Policy
+              </Text>
+              <Pressable style={modalStyle.closeButton} onPress={() => setModalVisible(false)}>
+                <AntDesign name="close" size={24} color={primaryColor} />
+              </Pressable>
+
+              <Text style={[textStyles.lg, { marginTop: 20 }]}>
+                Acknowledgment
+              </Text>
+              <Text style={[textStyles.lg, {textAlign: "justify", lineHeight: 32, marginTop: 8}]}>
+                These are the Terms and Conditions governing the use of this
+                Service and the agreement that operates between You and the
+                Company. These Terms and Conditions set out the rights and
+                obligations of all users regarding the use of the Service. Your
+                access to and use of the Service is conditioned on Your
+                acceptance of and compliance with these Terms and Conditions.
+                These Terms and Conditions apply to all visitors,
+              </Text>
+            </View>
+          </BlurView>
+        </Modal>
       </ScrollView>
     </>
   );
 };
 
 export default Signup;
+
+const modalStyle = StyleSheet.create({
+  container: {
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 28,
+  },
+
+  modal: {
+    width: "100%",
+    borderRadius: 24,
+    backgroundColor: "white",
+    borderColor: primaryColor,
+    borderWidth: 1,
+    padding: 20,
+  },
+
+  closeButton: {
+    position: "absolute",
+    right: 15,
+    top: 15,
+  },
+});
