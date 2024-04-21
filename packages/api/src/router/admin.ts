2324ec7c38f87a257772e12e86fa2809e6a3948f
Alexis Ken Alvarez <alexisken1432@gmail.com>
2024-04-21 12:14:24 +0800

    fix: reply and closed

--- packages/api/src/router/admin.ts
+++ packages/api/src/router/admin.ts
@@ -1,4 +1,5 @@
 import { TRPCError } from "@trpc/server";
+import nodemailer from "nodemailer";
 import { z } from "zod";
 
 import { createTRPCRouter, protectedProcedure } from "../trpc";
@@ -43,7 +44,7 @@ export const adminRouter = createTRPCRouter({
         const { count, error } = await ctx.supabase
           .from("tickets")
           .select("*", { count: "exact" })
-          .neq("status", "pending");
+          .neq("status", "open");
 
         if (error) {
           throw new TRPCError({
@@ -54,5 +55,99 @@ export const adminRouter = createTRPCRouter({
 
         return count;
       }
+    }),
+  getUsers: protectedProcedure.query(async ({ ctx }) => {
+    const { data, error } = await ctx.supabase.from("users").select("*");
+
+    if (error) {
+      throw new TRPCError({
+        message: "Failed to get users",
+        code: "BAD_REQUEST",
+      });
+    }
+
+    return data;
+  }),
+  getReports: protectedProcedure.query(async ({ ctx }) => {
+    const { data, error } = await ctx.supabase
+      .from("tickets")
+      .select("*, author(*)")
+      .eq("status", "open");
+
+    if (error) {
+      throw new TRPCError({
+        message: "Failed to get users",
+        code: "BAD_REQUEST",
+      });
+    }
+
+    return data;
+  }),
+  sendEmail: protectedProcedure
+    .input(
+      z.object({
+        toSend: z.string().email(),
+        message: z.string(),
+        id: z.number(),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      try {
+        const transporter = nodemailer.createTransport({
+          service: "gmail",
+          host: "smtp.gmail.com",
+          port: 465,
+          secure: true,
+          tls: {
+            ciphers: "SSLv3",
+            rejectUnauthorized: false,
+          },
+          auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+          },
+        });
+
+        await transporter.sendMail({
+          from: process.env.EMAIL_USER,
+          to: input.toSend,
+          subject: `QAVITE`,
+          html: `
+          <p>Message: ${input.message} </p>
+          `,
+        });
+
+      } catch (error) {
+        console.log(error);
+        throw new TRPCError({
+          message: "Failed to send email",
+          code: "BAD_REQUEST",
+        });
+      }
+
+      return true;
+    }),
+  closeTicket: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { error } = await ctx.supabase
+        .from("tickets")
+        .update({
+          status: "closed",
+        })
+        .eq("id", input.id);
+
+      if (error) {
+        throw new TRPCError({
+          message: "Failed to close ticket",
+          code: "BAD_REQUEST",
+        });
+      }
+
+      return true;
     }),
 });
