d34654ac92c9d9ac41c8e09b58fb001b08929b41
AlexisKenAlvarez <alexisken1432@gmail.com>
2024-05-22 12:48:05 +0800

    changes

--- apps/nextjs/src/component/Nav.tsx
+++ apps/nextjs/src/component/Nav.tsx
@@ -62,10 +62,7 @@ const Nav = () => {
           </ul>
         </div>
 
-        <button className="hover:bg-green1 flex w-full items-center gap-2 px-4 py-5 text-white transition-all duration-300 ease-in-out" onClick={async () => {
-          await supabase.auth.signOut()
-          router.refresh()
-        }}>
+        <button className="hover:bg-green1 flex w-full items-center gap-2 px-4 py-5 text-white transition-all duration-300 ease-in-out">
           <LogOut />
           <p className="">Logout</p>
         </button>
